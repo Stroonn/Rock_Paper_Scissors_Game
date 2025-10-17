cf7ffb845b2bd6195dc9ecc4c52c4525eab09147
Matheus <matheus.gato.gt98@gmail.com>
2025-10-17 18:24:58 +0000

    Implement game logic for Rock, Paper, Scissors including player and computer choices

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
 console.log("Hello world!");
+let humanscore = 0;
+let computerscore = 0;
+
 
 function getComputerChoice(){
     var options = ['rock', 'paper', 'scissors'];
@@ -8,4 +11,33 @@ function getComputerChoice(){
     return selectedoption;
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    var choice = prompt("Choose rock, paper or scissors: ");
+    choice = choice.toLowerCase();
+    return choice;
+}
+
+function PlayRound(){
+    var player = getHumanChoice();
+    var computer = getComputerChoice();
+
+    if(player === computer){
+        console.log("It's a tie! Both chose " + player);
+    }else if(
+        (player === 'rock' && computer === 'scissors') ||
+        (player === 'paper' && computer === 'rock') ||
+        (player === 'scissors' && computer === 'paper')
+    ){
+        humanscore++;
+        console.log("You win! " + player + " beats " + computer);
+    }else if(
+        (player === 'rock' && computer === 'paper') ||
+        (player === 'paper' && computer === 'scissors') ||
+        (player === 'scissors' && computer === 'rock')
+    ){
+        console.log("You lose! " + computer + " beats " + player);
+        computerscore++;
+    }
+}
+
+PlayRound();
