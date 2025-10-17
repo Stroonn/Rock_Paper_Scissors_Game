6ae49532c3f2cf22991bdae17ff043170c8afa8f
Matheus <matheus.gato.gt98@gmail.com>
2025-10-17 18:18:42 +0000

    Implement getComputerChoice function to randomly select rock, paper, or scissors

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
 console.log("Hello world!");
+
+function getComputerChoice(){
+    var options = ['rock', 'paper', 'scissors'];
+
+    var selectedoption = options[Math.ceil(Math.random() * options.length) - 1];
+
+    return selectedoption;
+}
+
+console.log(getComputerChoice());
