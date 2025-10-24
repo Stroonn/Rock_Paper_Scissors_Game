f8e3b3793e4dce6d66d3895aa91b7b70efd3cce8
Matheus <matheus.gato.gt98@gmail.com>
2025-10-24 20:48:35 +0000

    Refactor game logic and UI for Rock, Paper, Scissors game; add event listeners for player choices and implement scoring system

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-console.log("Hello world!");
+/*console.log("Hello world!");
 let humanscore = 0;
 let computerscore = 0;
 
@@ -55,4 +55,73 @@ function PlayGame(){
     }
 }
 
-PlayGame();
+PlayGame();*/
+
+let humanscore = 0;
+let computerscore = 0;
+
+const myButton = document.getElementById("rock");
+myButton.addEventListener("click", function () {
+    var computer = getComputerChoice();
+    console.log(computer);
+
+    if(computer == 'rock'){
+        alert("It's a tie! Both chose rock!");
+    }else if(
+        (computer === 'scissors')
+    ){
+        humanscore++;
+        alert("You win! Rock beats Scissors!");
+    }else if(
+        (computer === 'paper')
+    ){
+        alert("You lose! Paper beats Rock!");
+        computerscore++;
+    }
+});
+
+const myButton1 = document.getElementById("paper");
+myButton1.addEventListener("click", function () {
+    var computer = getComputerChoice();
+
+    if(computer == 'paper'){
+        alert("It's a tie! Both chose Paper!");
+    }else if(
+        (computer === 'rock')
+    ){
+        humanscore++;
+        alert("You win! Paper beats Rock!");
+    }else if(
+        (computer === 'scissors')
+    ){
+        alert("You lose! Scissors beats Paper!");
+        computerscore++;
+    }
+});
+
+const myButton2 = document.getElementById("scissors");
+myButton2.addEventListener("click", function () {
+    var computer = getComputerChoice();
+
+    if(computer == 'scissors'){
+        alert("It's a tie! Both chose Scissors!");
+    }else if(
+        (computer === 'paper')
+    ){
+        humanscore++;
+        alert("You win! Scissors beats Paper!");
+    }else if(
+        (computer === 'rock')
+    ){
+        alert("You lose! Rock beats Scissors!");
+        computerscore++;
+    }
+});
+
+function getComputerChoice(){
+    var options = ['rock', 'paper', 'scissors'];
+
+    var selectedoption = options[Math.ceil(Math.random() * options.length) - 1];
+
+    return selectedoption;
+}
